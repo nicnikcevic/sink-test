04354878b250b952adec9a3e91711adbcf74702d
Nic Nikcevic <nicnikcevic@gmail.com>
2025-01-18 14:21:52 -0500

    Initial Commit

---
+++ SinkTest/SinkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinkTestApp.swift
+//  SinkTest
+//
+//  Created by Nic Nikcevic on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SinkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SinkTestTests/SinkTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SinkTestTests.swift
+//  SinkTestTests
+//
+//  Created by Nic Nikcevic on 1/18/25.
+//
+
+import Testing
+@testable import SinkTest
+
+struct SinkTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
